da1080361725805cebd5ebab3aa0c1fd1cfc7188
jaaader <71683196+jaaader@users.noreply.github.com>
2020-10-25 14:28:12 -0500

    Update SkipList.java
    Edited constructor to include pred. fixed add(), isEmpty(), and chooseLevel(). added size() and findPred()

--- SkipList.java
+++ SkipList.java
@@ -12,6 +12,7 @@ import java.util.NoSuchElementException;
 public class SkipList<T extends Comparable<? super T>> {
     static final int PossibleLevels = 33;
     Entry <T> head, tail;
+    Entry <T>[] pred;
     int size;
 
     static class Entry<E> {
@@ -37,9 +38,10 @@ public class SkipList<T extends Comparable<? super T>> {
     public SkipList() { 
        head = new Entry(null, PossibleLevels);
        tail = new Entry(null, PossibleLevels);
+       pred = new Entry[PossibleLevels];
        size = 0;
 	 
-       for (int i = 0; i < 33; i++) // for all levels, head points to tail
+       for (int i = 0; i < PossibleLevels; i++) // for all levels, head points to tail
        {
            head.next[i] = tail;
        }
@@ -49,13 +51,23 @@ public class SkipList<T extends Comparable<? super T>> {
 
     // Add x to list. If x already exists, reject it. Returns true if new node is added to list
     public boolean add(T x) {
-        if (contains(x)) return false;
+        if (contains(x)) return false; // x exists in list, reject
         
-        else
+        else  // add if not in list
         {
-         int lvl = chooseLevel();
-         Entry<T> entry = new Entry(x,lvl);
-	 size ++;
+            int lvl = chooseLevel();
+            Entry<T> entry = new Entry(x,lvl);
+            findPred(x);
+            
+            for (int i = 0; i < lvl; i++)  // for all levels new entry is in, add it to list
+            {
+                entry.next[i] = pred[i].next[i]; //new entry points to value after pred
+                pred[i].next[i] = entry;         // pred now points to new entry
+                
+                
+            }
+            
+            size++;
           
           return true;  
         }
@@ -116,9 +128,9 @@ public class SkipList<T extends Comparable<? super T>> {
         }
     }
 
-    // Is the list empty?
+    // Is the list empty? Done by Jade Rodriguez.
     public boolean isEmpty() {
-        if (head.next[0] == tail) return true;
+        if (size == 0) return true;
         else return false;
     }
 
@@ -137,15 +149,31 @@ public class SkipList<T extends Comparable<? super T>> {
 	return null;
     }
 
-    // Return the number of elements in the list
+    // Return the number of elements in the list. Done by Jade Rodriguez
     public int size() {
-	return 0;
+	return size;
     }
 
     //Determines entry level when adding to list. Added by Andrew Kolkmeier
     public int chooseLevel(){
         Random r = new Random();
         int lvl = 1 + Integer.numberOfTrailingZeros(r.nextInt());
-        return min(lvl, PossibleLevels - 1);
+        return Math.min(lvl, PossibleLevels - 1);
     }
+    
+    //Helper method to find predecessor of x at each level. Added by Jade Rodriguez
+    public void findPred(T x){
+        Entry <T> p = head;
+        for (int i = PossibleLevels-1; i >= 0; i--)
+        {
+	    // find largest element less than x
+            while (p.next[i].element != null && ((T)p.next[i].element).compareTo(x) < 0){
+                p = p.next[i];
+            }
+	    //store value as pred;
+            pred[i]= p;
+        }
+    }
+    
+	
 }
